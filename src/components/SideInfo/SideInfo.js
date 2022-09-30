679dccc03990ae08fcf80f4f8551801314290f36
Iqbaleco <iqbaleco70@gmail.com>
2022-09-30 07:59:30 +0600

    added some new data

--- src/components/SideInfo/SideInfo.js
+++ src/components/SideInfo/SideInfo.js
@@ -10,6 +10,7 @@ const SideInfo = (props) => {
         exerciseTime = (exerciseTime + item.timeRequired);
     }
 
+
     return (
         <div className='side-info'>
 
@@ -20,14 +21,15 @@ const SideInfo = (props) => {
             <strong>Oxygen, Chattogram.</strong>
             <h2>Add a Break</h2>
             <div className='btn-break'>
-                <button onClick={() => handleAddToBreak}>30<strong>S</strong></button>
+                <button>30<strong>S</strong></button>
                 <button>25<strong>S</strong></button>
                 <button>40<strong>S</strong></button>
                 <button>50<strong>S</strong></button>
             </div>
             <h2>Exercise Details</h2>
             <h5>Exercise time: {exerciseTime}</h5>
-            <h5>Break time: {breakTime}</h5>
+            <h5>Break Time:</h5>
+            <button className='btn-activity'>Activity Completed</button>
 
 
         </div>
