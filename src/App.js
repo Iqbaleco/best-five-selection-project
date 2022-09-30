67d8f9e7409ccbc1094254cbbc6006cdbad2b7ce
Iqbaleco <iqbaleco70@gmail.com>
2022-09-30 10:18:08 +0600

    added some new data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import UltimatePC from './components/UltimatePC/UltimatePC';
 import InfoSection from './components/InfoSection/InfoSection';
 
+
 function App() {
   return (
     <div>
--- src/components/SideInfo/SideInfo.js
+++ src/components/SideInfo/SideInfo.js
@@ -10,6 +10,11 @@ const SideInfo = (props) => {
         exerciseTime = (exerciseTime + item.timeRequired);
     }
 
+    const notify = () => toast("Wow so easy !");
+    function handleBreakTime() {
+
+    };
+
 
     return (
         <div className='side-info'>
@@ -21,14 +26,14 @@ const SideInfo = (props) => {
             <strong>Oxygen, Chattogram.</strong>
             <h2>Add a Break</h2>
             <div className='btn-break'>
-                <button>30<strong>S</strong></button>
-                <button>25<strong>S</strong></button>
-                <button>40<strong>S</strong></button>
-                <button>50<strong>S</strong></button>
+                <button onClick={() => handleBreakTime()}>30<strong>S</strong></button>
+                <button onClick={() => handleBreakTime()}>25<strong>S</strong></button>
+                <button onClick={() => handleBreakTime()}>40<strong>S</strong></button>
+                <button onClick={() => handleBreakTime()}>50<strong>S</strong></button>
             </div>
             <h2>Exercise Details</h2>
             <h5>Exercise time: {exerciseTime}</h5>
-            <h5>Break Time:</h5>
+            <h5>Break Time:25 S</h5>
             <button className='btn-activity'>Activity Completed</button>
 
 
