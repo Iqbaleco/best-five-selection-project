c4613987f70539d7391fa725e415e5720ff2e198
Iqbaleco <iqbaleco70@gmail.com>
2022-09-30 11:09:25 +0600

    something added

--- src/components/SideInfo/SideInfo.js
+++ src/components/SideInfo/SideInfo.js
@@ -10,10 +10,17 @@ const SideInfo = (props) => {
         exerciseTime = (exerciseTime + item.timeRequired);
     }
 
-    const notify = () => toast("Wow so easy !");
-    function handleBreakTime() {
-
-    };
+    const BreakTime = () => {
+        const [breakTime, setBreakTime] = useState([]);
+        const [activeButton, setActiveButton] = useState("");
+        const handleClick = (e) => {
+            const { name } = e.target;
+            const buttonValue = e.target.innerHTML;
+            const numButtonValue = parseInt(buttonValue.slice(0, -1));
+            setBreakTime(numButtonValue)
+            setActiveButton(name)
+        }
+    }
 
 
     return (
@@ -26,10 +33,11 @@ const SideInfo = (props) => {
             <strong>Oxygen, Chattogram.</strong>
             <h2>Add a Break</h2>
             <div className='btn-break'>
-                <button onClick={() => handleBreakTime()}>30<strong>S</strong></button>
-                <button onClick={() => handleBreakTime()}>25<strong>S</strong></button>
-                <button onClick={() => handleBreakTime()}>40<strong>S</strong></button>
-                <button onClick={() => handleBreakTime()}>50<strong>S</strong></button>
+                <button name="1st" onClick={handleClick}>30s</button>
+                <button name="2nd" onClick={handleClick}>20s</button>
+                <button name="3rd" onClick={handleClick}>25s</button>
+                <button name="4th" onClick={handleClick}>15s</button>
+
             </div>
             <h2>Exercise Details</h2>
             <h5>Exercise time: {exerciseTime}</h5>
